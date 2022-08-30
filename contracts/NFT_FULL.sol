9e3966cd9b1466e1ec1b5086ace4a22a59b2a7ba
Brunvelop <brunvelop@gmail.com>
2022-08-30 11:57:18 +0200

    feat: add new example contract with more options

---
+++ contracts/NFT_FULL.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Pixt is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    uint256 public MAX_SUPPLY;
+    uint256 public MAX_MINT_PER_WALLET;
+    bool public isPublicMitEnabled;
+    string internal baseTokenUri;
+    address payable public withdrawWallet;
+
+    constructor() payable ERC721("Pixt", "PXT") {
+        MAX_SUPPLY = 333;
+        MAX_MINT_PER_WALLET = 3;
+        _tokenIdCounter.increment();
+        // set witdraw wallet address
+    }
+
+    function setIsPublicMintEnabled(bool _isPublicMitEnabled) external onlyOwner {
+        isPublicMitEnabled = _isPublicMitEnabled;
+    }
+
+    function setBaseTokenUri(string calldata _baseTokenUri) external onlyOwner {
+        baseTokenUri = _baseTokenUri;
+    }
+
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        require(_exists(_tokenId), 'Token does not exist!');
+        return string(abi.encodePacked(baseTokenUri, Strings.toString(_tokenId),'.json'));
+    }
+
+    function totalSupply() external  view returns (uint256) {
+      return MAX_SUPPLY;
+    }
+
+    function withdraw() external onlyOwner {
+        (bool success, ) = withdrawWallet.call{ value: address(this).balance }('');
+        require(success,'Withdraw failed');
+    }
+
+    function freeMint() public payable {
+        require(msg.value == 0.1);
+        require(isPublicMitEnabled, 'Minting is not enabled');
+        require(balanceOf(msg.sender) < MAX_MINT_PER_WALLET, "Max Mint per wallet reached");
+        require(_tokenIdCounter.current() <= MAX_SUPPLY , "I'm sorry we reached the cap");
+
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(msg.sender, tokenId);
+    }
+}
